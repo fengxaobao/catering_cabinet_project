b6803d354f9befcce70232271faa7e26c63094ec
xiaobao.feng <xiaobao.feng@ishansong.com>
2020-07-06 14:54:41 +0800

    commit

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import Timer from 'Timer'
+class HomePage extends React.Component {
+    render() {
+        return <div>
+            <h1>Home page</h1>;
+            {/*<Timer></Timer>*/}
+        </div>
+    }
+}
+
+export default HomePage
